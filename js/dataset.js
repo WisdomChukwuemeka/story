2d9968a745f52bbdac53d980e408e780d881f4ee
wisdom chukwuemeka <wisdomchukwuemeka97@gmail.com>
2025-04-08 00:31:49 +0100

    code adjustment

---
+++ js/dataset.js
@@ -0,0 +1,6 @@
+export const Data = [
+    {id: 1, name: "Luke amstrong", no_book: 12, rating: 3.5, sold: "40%", genre: "Cooking", image: ("./story/src/imagesfolder/girlone.png"), bicon: "heart bi-heart", bicons: "redheart bi-heart"},
+    {id: 2, name: "Mattew peterson", no_book: 24, rating: 3.5, sold: "63%", genre: "Marriage", image: ("./story/src/imagesfolder/second.png"), bicon: "heart bi-heart", bicons: "redheart bi-heart"},
+    {id: 3, name: "Stella abigail", no_book: 5, rating: 1.5, sold: "2%", genre: "Education", image: ("./story/src/imagesfolder/braids.png"), bicon: "heart bi-heart", bicons: "redheart bi-heart"},
+    {id: 4, name: "Lugard aduak", no_book: 31, rating: 4, sold: "70%", genre: "Abuse", image: ("./story/src/imagesfolder/man.png"), bicon: "heart bi-heart", bicons: "redheart bi-heart"}
+]
--- pages/home.jsx
+++ pages/home.jsx
@@ -1,30 +1,60 @@
-export const Home = () => {
-    return(
-        <>
-            <div className="home-container">
-                <div className="home-image">
-                    <div className="title">
-                        <div className="title-two">
-                        <h1>Tell your story today!</h1>
-                        <p>Everyone has a story to tell, the world needs to hear your story.</p>
-                        <div className="started">
-                            <div className="started-container">
-                                <div>Get Started <span><i class="bi bi-arrow-right-circle-fill"></i></span> </div>
+import {Data} from "../js/dataset.js"    
+
+
+    export const Home = () => {
+        
+        return(
+            <>
+                <div className="home-container">
+                    <div className="home-image">
+                        <div className="title">
+                            <div className="title-two">
+                            <h1>Tell your story today!</h1>
+                            <p>Everyone has a story to tell, the world needs to hear your story.</p>
+                            <div className="started">
+                                <div className="started-container">
+                                    <div>Get Started <span><i class="bi bi-arrow-right-circle-fill"></i></span> </div>
+                                </div>
                             </div>
+                            </div>  
                         </div>
-                        </div>  
+                    </div>
+
+                    <div className="home-image-bottom">
+                        <div>More...</div>
+                    </div>
+
+                </div>
+
+                <div className="second-home-container">
+                    <div className="join">
+                        <div>
+                            <h1>Your story matters</h1>
+                            <p>Unlock the power of your story! 
+                                Every experience, challenge, and triumph you’ve faced can inspire others to take action and pursue their dreams. 
+                                Share your journey and watch as your words ignite hope and motivation in those who need it most. Your story could 
+                                be the spark that changes a life!
+                                </p>
+                        </div>
+                    </div>
+                    <div className="second-section">
+                        {Data.map((item) => (
+                            <div className="sub-section" key={item.id}>
+                                <div className="item-image">
+                                <img src={item.image} alt="" />
+                                <span></span>
+                                </div>
+                                <div className="info">
+                                <h3>{item.name}</h3>
+                                <p>Books: {item.no_book}</p>
+                                <p>Sold: {item.sold}</p>
+                                <p>Genre: {item.genre}</p>
+                                </div>
+                                
+                            </div>
+                        ))}
                     </div>
                 </div>
-
-                <div className="home-image-bottom">
-                    <div>More...</div>
-                </div>
-
-            </div>
-
-            <div>
-                kdkdkd
-            </div>
-        </>
-    )
-}
+            </>
+        )
+    }
